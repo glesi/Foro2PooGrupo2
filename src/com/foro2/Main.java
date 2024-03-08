4cac354310756e72fa60fd1a52f49db2c31a42fd
glesi <glesi.mayandy@gmail.com>
2024-03-08 15:11:30 -0600

    Se agrega proyecto base, clase Main y clase Inventario
    La clase inventario la usaremos para pedir los datos al usuario para cada tipo de dispositivo y para mostrar los dispositivos segun lo enviado por el menu.

---
+++ src/com/foro2/Main.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.foro2;
+
+import com.foro2.procesos.Equipo;
+import com.foro2.procesos.Inventario;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author glesi
+ */
+public class Main {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        List<Equipo> equipos = new ArrayList<>();
+        Inventario inventario = new Inventario();
+        
+        //Agregar menu con las diferentes opciones
+        
+    }
+    
+}
