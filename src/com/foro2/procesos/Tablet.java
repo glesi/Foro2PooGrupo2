36fffc9471f018c0aa044ccca895d4d8d03bdc2c
Fabricio Clemente <144067212+Fabclemente@users.noreply.github.com>
2024-03-15 22:40:59 -0600

    Tablet Class Agregada
    Codigo Tablet Agregado

--- src/com/foro2/procesos/Tablet.java
+++ src/com/foro2/procesos/Tablet.java
@@ -7,12 +7,56 @@ package com.foro2.procesos;
 
 /**
  *
- * @author glesi
+ * @author Fab
  */
 public class Tablet extends Equipo {
     
-    public Tablet(String fabricante, String modelo, String microprocesador) {
+    private String diagonalPantalla;
+    private double capacitivaResistiva;
+    private double tamañoMemoriaNAND;
+    private double sistemaOperativo;
+    
+    public Tablet(String tamañoPantalla, double capacitivaResistiva, double tamañoMemoriaNAND, double sistemaOperativo, String fabricante, String modelo, String microprocesador) {
         super(fabricante, modelo, microprocesador);
+        this.diagonalPantalla = tamañoPantalla;
+        this.capacitivaResistiva = capacitivaResistiva;
+        this.tamañoMemoriaNAND = tamañoMemoriaNAND;
+        this.sistemaOperativo = sistemaOperativo;
     }
     
-}
+    public String gettamañoPatalla () {
+        return diagonalPantalla;
+    }
+    
+    public void settamañoPantalla (String tamañoPantalla){
+        this.diagonalPantalla = tamañoPantalla;
+    }
+    
+    public double getcapacitivaResistiva (){
+        return capacitivaResistiva;
+    }
+    
+    public void setcapacitivaResistiva (double capacitivaResistiva){
+        this.capacitivaResistiva = capacitivaResistiva;
+    }
+    
+    public double gettamañoMemoriaNAND (){
+        return tamañoMemoriaNAND;
+    }
+    
+    public void settamañoMemoriaNAND (double tamañoMemoriaNAND) {
+        this.tamañoMemoriaNAND = tamañoMemoriaNAND;
+    }
+    
+    public double getsistemaOperativo (){
+        return sistemaOperativo;
+    } 
+    
+    public void setsistemaOperativo (double sistemaOperativo){
+        this.sistemaOperativo = sistemaOperativo;
+    }
+    
+    @Override
+    public String toString (){
+        return "Tablet{fabricante=" + fabricante + ", modelo=" + modelo + ", microprocesador=" + microprocesador + ", diagonalPantalla=" + diagonalPantalla + ", capacitivaResistiva=" + capacitivaResistiva + ",tamañoMemoriaNAND=" + tamañoMemoriaNAND + ", sistemaOperativo=" + sistemaOperativo + '}';
+    } 
